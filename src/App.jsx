dd81219af17bca0440fe9d08c962e6fdd8f3042c
inhohyun <inhohyun36@gmail.com>
2024-07-13 17:43:02 +0900

    feat: 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Routes, Route, Link, useNavigate} from 'react-router-dom'
+import Home from './pages/Home'
+import New from './pages/New'
+import Diary from './pages/Diary'
+import Notfound from './pages/Notfound'
 function App() {
-  const [count, setCount] = useState(0)
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav('/new')
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/new"}>New</Link>
+      <Link to={"/diary"}>Diary</Link>
+    </div>
+    <button onClick={onClickButton}>New 페이지로 이동</button>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/new' element={<New/>}></Route>
+      <Route path='/diary/:id' element={<Diary/>}></Route>
+      <Route path='*' element={Notfound}></Route>
+    </Routes>
     </>
   )
 }
